75497c4ea590cd8fb80755d2b7b5319635900669
molomojc <49805877+molomojc@users.noreply.github.com>
2025-03-24 16:36:58 +0200

    Update Credits.jsx

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -14,7 +14,7 @@ const Credits = () => {
 		>
 			<span>Developed by</span>
 			<motion.a whileTap={{scale: 0.9}} className='Credits__linkwrp' href={GITHUB_BASE_URL} target='_blank' rel='noreferrer'>
-				<span> Th3Wall</span>
+				<span> Molomo</span>
 				<img className='Credits__avatar' src={GITHUB_AVATAR_URL} alt='Credits Avatar' />
 			</motion.a>
 		</motion.footer>
