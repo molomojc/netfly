9e8849823a553031ee792528fff96fa4dcb1dbfb
molomojc <molomojacob8@gmail.com>
2025-04-07 15:07:33 +0300

    next button

--- src/components/PlayMovie/PlayMovie.jsx
+++ src/components/PlayMovie/PlayMovie.jsx
@@ -1,27 +1,38 @@
-import './PlayMovie.scss';
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation} from "react-router-dom"; // Replace useNavigate with useHistory
+import { FaForward } from "react-icons/fa";
+import "./PlayMovie.scss"; // Import your styles here
 
 const PlayMovie = () => {
     const location = useLocation();
-    const { id, isTv, Season, Episode} = location.state || {};
-    const [currentServer, setCurrentServer] = useState(1); // Default: Server 1
- console.log("Season Number", Season);
-    console.log("Episode Number", Episode);
-   
+  //  const history = useHistory(); // Replace navigate with history
+    const { id, isTv, Season: initialSeason = 1, Episode: initialEpisode = 1 } = location.state || {};
+    const [currentServer, setCurrentServer] = useState(1);
+    const [currentSeason] = useState(initialSeason);
+    const [currentEpisode, setCurrentEpisode] = useState(initialEpisode);
+
+    useEffect(() => {
+        // When currentEpisode or currentSeason changes, re-render iframe
+    }, [currentEpisode, currentSeason]);
+
+    const handleNextEpisode = () => {
+        setCurrentEpisode((prev) => prev + 1);
+    };
+
     const servers = {
-        1: isTv ? `https://vidsrc.cc/v2/embed/tv/${id}/${Season}/${Episode}` 
-               : `https://vidsrc.cc/v2/embed/movie/${id}`,
-        2: isTv ? `https://vidsrc.xyz/embed/tv/${id}/${Season}-${Episode}` 
-               : `https://vidsrc.xyz/embed/movie/${id}`,
-        3: isTv ? `https://www.2embed.cc/embedtv/${id}&s=${Season}&e=${Episode}` 
-               : `https://www.2embed.cc/embed/${id}`
+        1: isTv
+            ? `https://vidsrc.cc/v2/embed/tv/${id}/${currentSeason}/${currentEpisode}`
+            : `https://vidsrc.cc/v2/embed/movie/${id}`,
+        2: isTv
+            ? `https://vidsrc.xyz/embed/tv/${id}/${currentSeason}-${currentEpisode}`
+            : `https://vidsrc.xyz/embed/movie/${id}`,
+        3: isTv
+            ? `https://www.2embed.cc/embedtv/${id}&s=${currentSeason}&e=${currentEpisode}`
+            : `https://www.2embed.cc/embed/${id}`,
     };
 
     return (
         <div className="PlayMovie__wrp">
-           
-        
             <div className="video-container">
                 <iframe
                     src={servers[currentServer]}
@@ -29,8 +40,7 @@ const PlayMovie = () => {
                     allowFullScreen
                     frameBorder="0"
                     title={`${isTv ? "TV Show" : "Movie"} Player`}
-
-                    style={{ width: "100%", height: "680px", overflow: "hidden", marginTop: "52px" }}
+                    style={{ width: "100%", height: "620px", overflow: "hidden", marginTop: "62px" }}
                 />
             </div>
 
@@ -44,6 +54,12 @@ const PlayMovie = () => {
                         Server {serverNum}
                     </button>
                 ))}
+
+                {isTv && (
+                    <button className="next-episode-button" onClick={handleNextEpisode}>
+                        <FaForward /> Next Episode
+                    </button>
+                )}
             </div>
         </div>
     );
